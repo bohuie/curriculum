bb69ac51991b32e8d3c82ce69b2f66f3f9b949f2
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2021-07-26 13:34:43 -0600

    Changes to outcome_maps

--- resources/views/programs/wizard/step1.blade.php
+++ resources/views/programs/wizard/step1.blade.php
@@ -17,7 +17,7 @@
 
                     <h6 class="card-subtitle mb-4 text-center lh-lg">
                         Program-level learning outcomes (PLOs) are the knowledge, skills and attributes that students are expected to attain by the end of a program of study.
-                        You can add, edit and delete program outcomes. <strong>It is recommended that a program has 6 - 8 PLOs max</strong>. You can also add program outcome categories to group outcomes (optional).                    
+                        You can add, edit and delete program outcomes. <strong>It is recommended that a program has 6 - 8 PLOs max</strong>. You can also add program outcome categories to group outcomes.                    
                     </h6>
 
 
--- resources/views/programs/wizard/step3.blade.php
+++ resources/views/programs/wizard/step3.blade.php
@@ -386,7 +386,7 @@
                                         </div>
 
                                         <div class="form-group row">
-                                            <label for="course_section" class="col-md-3 col-form-label text-md-right"><span class="requiredField">*</span>Course
+                                            <label for="course_section" class="col-md-3 col-form-label text-md-right">Course
                                                 Section</label>
 
                                             <div class="col-md-4">
