290c026d7aef13baf8a6ea155d7360f18375378f
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2021-08-31 12:02:23 -0600

    Updated Course Number to only accept numbers, and remove leading zeros

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -93,9 +93,18 @@ class CourseController extends Controller
             ]);
 
         $course = new Course;
-        // TODO Update name of column program-id to ministry standards 
         $course->course_title = $request->input('course_title');
-        $course->course_num = $request->input('course_num');
+        // remove leading zeros from course number
+        $CNum = $request->input('course_num');
+        for ($i = 0; $i < strlen($CNum); $i++) {
+            if ($CNum[$i] == '0') {
+                $CNum = ltrim($CNum, $CNum[$i]);
+            } else {
+                // Found a value that's not '0'
+                break;
+            }
+        }
+        $course->course_num = $CNum;
         $course->course_code =  strtoupper($request->input('course_code'));
         // status of mapping process
         $course->status = -1;
@@ -278,7 +287,17 @@ class CourseController extends Controller
             ]);
 
         $course = Course::where('course_id', $course_id)->first();
-        $course->course_num = $request->input('course_num');
+        // remove leading zeros from course number
+        $CNum = $request->input('course_num');
+        for ($i = 0; $i < strlen($CNum); $i++) {
+            if ($CNum[$i] == '0') {
+                $CNum = ltrim($CNum, $CNum[$i]);
+            } else {
+                // Found a value that's not '0'
+                break;
+            }
+        }
+        $course->course_num = $CNum;
         $course->course_code = strtoupper($request->input('course_code'));
         $course->course_title = $request->input('course_title');
         $course->required = $request->input('required');
