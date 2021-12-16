869a8cecc5869e7ef3789d89af29007a80abef37
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2021-12-16 14:37:56 -0800

    Updated URL's for Emails

--- resources/views/emails/notifyProgramAdmin.blade.php
+++ resources/views/emails/notifyProgramAdmin.blade.php
@@ -4,7 +4,7 @@
 
 {{$user_name}} has invited you to collaborate on the program: {{$program_title}} from the Department of {{$program_dept}}
 
-@component('mail::button', ['url' => env('LOGIN_URL')])
+@component('mail::button', ['url' => 'http://127.0.0.1:8000/login'])
 Log In and See Program
 @endcomponent
 
