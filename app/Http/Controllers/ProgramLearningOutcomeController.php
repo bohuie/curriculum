e794ad27f0687555aadb07c11d839e375e50c55a
Kieran Adams <kieranadamslink1@gmail.com>
2022-01-03 14:27:06 +0200

    Updated step 1 of program wizard with the ability to crud many plo categories and plos, plus minor improvements to all modals

--- app/Http/Controllers/ProgramLearningOutcomeController.php
+++ app/Http/Controllers/ProgramLearningOutcomeController.php
@@ -8,6 +8,8 @@ use App\Models\ProgramLearningOutcome;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class ProgramLearningOutcomeController extends Controller
 {
@@ -44,40 +46,68 @@ class ProgramLearningOutcomeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate request data 
         $this->validate($request, [
-            
-            'plo'=> 'required', 
-            ]);
+            'program_id'=> 'required',
+        ]);
 
-        $plo = new ProgramLearningOutcome;
-        $plo->pl_outcome = $request->input('plo');
-        $plo->plo_shortphrase = $request->input('title');
-        $plo->program_id = $request->input('program_id');
-
-        $program = Program::find($request->input('program_id'));
-        // get users name for last_modified_user
-        $user = User::find(Auth::id());
-        $program->last_modified_user = $user->name;
-        $program->save();
-
-        CourseProgram::where('program_id', $request->input('program_id'))->update(['map_status' => 0]);
-
-        if($request->has('category')){
-            $plo->plo_category_id = $request->input('category');
+        try {
+            $programId = $request->input('program_id');
+            // get this program
+            $program = Program::find($programId);
+            // get the current plos, their shortphrases and categories
+            $currentPLOs = $request->input('current_pl_outcome');
+            $currentPLOShortphrases = $request->input('current_pl_outcome_short_phrase');
+            $currentPLOCategories = $request->input('current_plo_category');
+            // get the new plos, their shortphrases and their categories
+            $newPLOs = $request->input('new_pl_outcome');    
+            $newPLOShortphrases = $request->input('new_pl_outcome_short_phrase');
+            $newPLOCategories = $request->input('new_plo_category');
+            // get the saved plos for this program
+            $plos = $program->programLearningOutcomes;
+            // update current plos
+            foreach ($plos as $plo) {
+                if (array_key_exists($plo->pl_outcome_id, $currentPLOs)) {
+                    // save and update plo 
+                    $plo->pl_outcome = $currentPLOs[$plo->pl_outcome_id];
+                    $plo->plo_shortphrase = $currentPLOShortphrases[$plo->pl_outcome_id];
+                    $plo->plo_category_id = $currentPLOCategories[$plo->pl_outcome_id];
+                    $plo->save();
+                } else {
+                    // remove plo from program 
+                    $plo->delete();
+                }
+            }
+            // add new plos
+            if ($newPLOs) {
+                foreach ($newPLOs as $index => $plo) {
+                    $newPLO = new ProgramLearningOutcome;
+                    $newPLO->pl_outcome = $plo;
+                    $newPLO->plo_shortphrase = $newPLOShortphrases[$index];
+                    $newPLO->plo_category_id = $newPLOCategories[$index];
+                    $newPLO->program_id = $programId;
+                    $newPLO->save();
+                }
+                // new unmapped plos added, update map_status
+                CourseProgram::where('program_id', $request->input('program_id'))->update(['map_status' => 0]);
+            }
+            // update which user last modified this program
+            $user = User::find(Auth::id());
+            $program->last_modified_user = $user->name;
+            $program->save();
+            $request->session()->flash('success','Your PLO categories were updated successfully!');
+        } catch (Throwable $exception) {
+            $message = 'There was an error updating your program learning outcomes';
+            Log::error($message . ' ...\n');
+            Log::error('Code - ' . $exception->getCode());
+            Log::error('File - ' . $exception->getFile());
+            Log::error('Line - ' . $exception->getLine());
+            Log::error($exception->getMessage());
+            $request->session()->flash('error', $message);
+        } finally { 
+            return redirect()->route('programWizard.step1', $request->input('program_id'));
         }
-        
-        if($plo->save()){
-            // update courses 'updated_at' field
-            $program = Program::find($request->input('program_id'));
-            $program->touch();
 
-            $request->session()->flash('success', 'New program learning outcome saved');
-        }else{
-            $request->session()->flash('error', 'There was an error adding the program learning outcome');
-        }
-        
-        return redirect()->route('programWizard.step1', $request->input('program_id'));
     }
 
     /**
