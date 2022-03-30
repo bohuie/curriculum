0c49b7b4da9f86a1f1e7b702544e4aa1b34bbf0b
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-03-30 15:21:36 -0700

    merge fixes

--- resources/views/emails/notifyProgramAdmin.blade.php
+++ resources/views/emails/notifyProgramAdmin.blade.php
@@ -1,15 +1,10 @@
 @component('mail::message')
 
 # You have been invited to collaborate on a program
-@if ($program_dept)
-    {{$user_name}} has invited you to collaborate on the program: {{$program_title}} from the Department of {{$program_dept}}
-@else 
-    {{$user_name}} has invited you to collaborate on the program: {{$program_title}}
-@endif
 
-{{$user_name}} has invited you to collaborate on the program: {{$program_title}}.
+{{$user_name}} has invited you to collaborate on the program: {{$program_title}}
 
-@component('mail::button', ['url' => 'http://127.0.0.1:8000/login'])
+@component('mail::button', ['url' => 'https://curriculum.ok.ubc.ca/login'])
 Log In and See Program
 @endcomponent
 
