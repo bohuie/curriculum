24a9018802bf6e4db197761723e458e875d87868
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-04-05 11:39:35 -0700

    Removed XSS middleware
    This middleware group is redundant and only restricts what the user can input.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         
-        'XSS' => [\App\Http\Middleware\XSS::class,],
+        // 'XSS' => [\App\Http\Middleware\XSS::class,],
     ];
 
     /**
--- app/Http/Middleware/XSS.php
+++ app/Http/Middleware/XSS.php
@@ -5,6 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
+// Most likely should be removed. I have commented this out under the Kernel middleware group.
+// Laravel has built in XSS protection built in and this is redundant.
+// if the user uses '<>' in any of the input fields then it will be removed, which is not ideal.
 class XSS {
     /**
      * Handle an incoming request.
