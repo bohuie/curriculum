3be76fd4c504744bffcd2fa037431fbb28f8f0e4
Matthew Penner <mpenner.1997@hotmail.com>
2022-04-20 14:06:55 -0700

    Revert op_id
    Revert op_ids

--- database/seeders/OptionalPrioritiesSeeder.php
+++ database/seeders/OptionalPrioritiesSeeder.php
@@ -230,62 +230,62 @@ class OptionalPrioritiesSeeder extends Seeder
         $opp->save();
 		
         $opp = new OptionalPriorities;
-        $opp->op_id = 18;
+        $opp->op_id = 19;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-2-inspiring-spaces/\" target=\"_blank\">Strategy 2: </a>
                                         Inspiring Spaces: Create welcoming physical and virtual spaces to advance collaboration, innovation and community development.";
         $opp->save();
 		
         $opp = new OptionalPriorities;
-        $opp->op_id = 19;
+        $opp->op_id = 28;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-11-education-renewal/\" target=\"_blank\">Strategy 11: </a>
                                         Education Renewal: Facilitate sustained program renewal and improvements in teaching effectiveness.";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 20;
+        $opp->op_id = 29;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-12-program-redesign/\" target=\"_blank\">Strategy 12: </a>
                                         Program Redesign: Reframe undergraduate academic program design in terms of learning outcomes and competencies.";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 21;
+        $opp->op_id = 30;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-13-practical-learning/\" target=\"_blank\">Strategy 13: </a>
                                         Practical Learning: Expand experiential, work-integrated and extended learning opportunities for students, faculty, staff and alumni.";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 22;
+        $opp->op_id = 31;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-14-interdisciplinary-education/\" target=\"_blank\">Strategy 14: </a>
                                         Interdisciplinary Education: Facilitate the development of integrative, problem-focused learning.";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 23;
+        $opp->op_id = 32;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-15-student-experience/\" target=\"_blank\">Strategy 15: </a>
                                         Student Experience: Strengthen undergraduate and graduate student communities and experience.";
         $opp->save();
         $opp = new OptionalPriorities;
-        $opp->op_id = 24;
+        $opp->op_id = 33;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-16-public-relevance/\" target=\"_blank\">Strategy 16: </a>
                                         Public Relevance: Deepen the relevance and public impact of UBC research and education.";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 25;
+        $opp->op_id = 34;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-17-indigenous-engagement/\" target=\"_blank\">Strategy 17: </a>
                                         Indigenous Engagement: Support the objectives and actions of the renewed Indigenous Strategic Plan.";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 26;
+        $opp->op_id = 35;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-18-alumni-engagement/\" target=\"_blank\">Strategy 18: </a>
                                         Alumni Engagement: Reach, inspire and engage alumni through lifelong enrichment, consistent with the alumniUBC strategic plan,
@@ -293,164 +293,164 @@ class OptionalPrioritiesSeeder extends Seeder
         $opp->save();
 		
         $opp = new OptionalPriorities;
-        $opp->op_id = 27;
+        $opp->op_id = 37;
         $opp->subcat_id = 3;
         $opp->optional_priority = "<a href=\"https://strategicplan.ubc.ca/strategy-20-co-ordinated-engagement/\" target=\"_blank\">Strategy 20: </a>
                                         Co-ordinated Engagement: Co-create with communities the principles and effective practices of engagement, and establish supporting infrastructure.";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 28;
+        $opp->op_id = 38;
         $opp->subcat_id = 4;
         $opp->optional_priority = "Continuing education programs";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 29;
+        $opp->op_id = 39;
         $opp->subcat_id = 4;
         $opp->optional_priority = "Offer hybrid pedagogies";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 30;
+        $opp->op_id = 40;
         $opp->subcat_id = 4;
         $opp->optional_priority = "Professional programs in health and technology";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 31;
+        $opp->op_id = 41;
         $opp->subcat_id = 4;
         $opp->optional_priority = "Increase graduate student training";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 32;
+        $opp->op_id = 42;
         $opp->subcat_id = 4;
         $opp->optional_priority = "Leverage new academic and/or research space";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 33;
+        $opp->op_id = 43;
         $opp->subcat_id = 4;
         $opp->optional_priority = "Increased community engagement";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 34;
+        $opp->op_id = 44;
         $opp->subcat_id = 5;
         $opp->optional_priority = "Integration of Indigenous histories, experiences, worldviews and knowledge systems";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 35;
+        $opp->op_id = 45;
         $opp->subcat_id = 5;
         $opp->optional_priority = "Inclusion of substantive content that explores histories and identifies how Indigenous issues intersect with the field of study";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 36;
+        $opp->op_id = 46;
         $opp->subcat_id = 5;
         $opp->optional_priority = "Inclusion of Indigenous people for the development and offering of the curriculum";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 37;
+        $opp->op_id = 47;
         $opp->subcat_id = 5;
         $opp->optional_priority = "Continue to partner with Indigenous communities locally and globally";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 38;
+        $opp->op_id = 48;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Climate justice education";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 39;
+        $opp->op_id = 49;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Climate research";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 40;
+        $opp->op_id = 50;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Content on Indigenous rights, content, history, and culture";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 41;
+        $opp->op_id = 51;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Environmental and sustainability education";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 42;
+        $opp->op_id = 52;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Content from Indigenous scholars and communities and/or equity-seeking and marginalized groups";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 43;
+        $opp->op_id = 53;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Inclusion of de-colonial approaches to science through Indigenous and community traditional knowledge and \"authorship\"";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 44;
+        $opp->op_id = 54;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Knowledge, awareness and skills related to the relationship between climate change and food systems";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 45;
+        $opp->op_id = 55;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Climate-related mental health content";
         $opp->save();
         
         $opp = new OptionalPriorities;
-        $opp->op_id = 46;
+        $opp->op_id = 56;
         $opp->subcat_id = 6;
         $opp->optional_priority = "Applied learning opportunities grounded in the personal, local and regional community (e.g. flood and wildfire impacted communities in BC)";
         $opp->save();
 		
 		$opp = new OptionalPriorities;
-        $opp->op_id = 47;
+        $opp->op_id = 61;
         $opp->subcat_id = 7;
         $opp->optional_priority = "Degree Requirements: Incorporate equity, diversity, and inclusion skills and competencies info degree requirements.";
         $opp->save();
 		
 		$opp = new OptionalPriorities;
-        $opp->op_id = 48;
+        $opp->op_id = 62;
         $opp->subcat_id = 7;
         $opp->optional_priority = "Dialogue for Engagement: Facilitate and provide opportunities for dialogue and conversation around sensitive topics at UBC and beyond. Build conflict engagement skills and practices among all members of UBC’s community to equip people for working across differences.";
         $opp->save();
 		
 		$opp = new OptionalPriorities;
-        $opp->op_id = 49;
+        $opp->op_id = 63;
         $opp->subcat_id = 7;
         $opp->optional_priority = "EDI Curriculum and Program Requirements: Embed equity and inclusion education into curriculum and program requirements for all students that incorporates intercultural understanding, empathy and mutual respect.";
         $opp->save();
 		
 		$opp = new OptionalPriorities;
-        $opp->op_id = 50;
+        $opp->op_id = 64;
         $opp->subcat_id = 7;
         $opp->optional_priority = "Student Learning: Review and improve mechanisms to ensure that student perspectives on the inclusiveness of their learning experiences are integrated into the improvement of teaching.";
         $opp->save();
 		
 		$opp = new OptionalPriorities;
-        $opp->op_id = 51;
+        $opp->op_id = 65;
         $opp->subcat_id = 8;
         $opp->optional_priority = "Mental Health & Resilience: Enhance mental health literacy to create a supportive campus culture and equipping our community with the skills needed to live well and improve their health, by creating environments that support them.";
         $opp->save();
 		
 		$opp = new OptionalPriorities;
-        $opp->op_id = 52;
+        $opp->op_id = 66;
         $opp->subcat_id = 8;
         $opp->optional_priority = "Social Connection: Encourage everyone to work, learn, collaborate, and build connections in ways that are meaningful to them. Strong and lasting social and friendship connections are critical to our wellbeing.";
         $opp->save();
 		
 		$opp = new OptionalPriorities;
-        $opp->op_id = 53;
+        $opp->op_id = 67;
         $opp->subcat_id = 8;
         $opp->optional_priority = "Physical Activity: Produce accessible programs, policies, and facilities that support physical activity for all ages and abilities.";
         $opp->save();
@@ -460,13 +460,13 @@ class OptionalPrioritiesSeeder extends Seeder
 
         // UBC Mandate 2021
         $opp = new OptionalPriorities();
-        $opp->op_id = 54;
+        $opp->op_id = 57;
         $opp->subcat_id = 1;
         $opp->optional_priority = `Incorporation of the Declaration on the Rights of Indigenous Peoples Act and Calls to Action of the Truth and Reconciliation Commission <a href="http://trc.ca/assets/pdf/Calls_to_Action_English2.pdf" target="_blank">( <i class="bi bi-box-arrow-up-right"></i> More Information can be found here)</a>`;
         $opp->year = 2021;
 
         $opp = new OptionalPriorities();
-        $opp->op_id = 55;
+        $opp->op_id = 58;
         $opp->subcat_id = 1;
         $opp->optional_priority = `Align with CleanBC's plan to a protect our communities towards a more sustainable future
         <a href="https://cleanbc.gov.bc.ca/" target="_blank">( <i class="bi bi-box-arrow-up-right"></i> 
@@ -474,14 +474,14 @@ class OptionalPrioritiesSeeder extends Seeder
         $opp->year = 2021;
 
         $opp = new OptionalPriorities();
-        $opp->op_id = 56;
+        $opp->op_id = 59;
         $opp->subcat_id = 1;
         $opp->optional_priority = `Work with communities, employers and industry to implement skills training towards career opportunities in BC <a href="https://students.ok.ubc.ca/career-experience/faculty-workshops/" target="_blank">( <i class="bi bi-box-arrow-up-right"></i> More 
         Information can be found here)</a>`;
         $opp->year = 2021;
 
         $opp = new OptionalPriorities();
-        $opp->op_id = 57;
+        $opp->op_id = 60;
         $opp->subcat_id = 1;
         $opp->optional_priority = `Offer programming aligned with <a href="https://students.ok.ubc.ca/career-experience/faculty-workshops/" target="_blank"><i class="bi bi-box-arrow-up-right"></i> high opportunity and priority occupations</a> (such as trades, technology, early childhood educators and health)`
         ;
