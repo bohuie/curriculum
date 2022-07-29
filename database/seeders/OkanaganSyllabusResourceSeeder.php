ff8ba6c9946035999b0dae8c164b65191d01214c
mogden8 <michaelogden8@gmail.com>
2022-07-29 15:06:33 -0700

    Updated OKSyllabusResource Seeder

--- database/seeders/OkanaganSyllabusResourceSeeder.php
+++ database/seeders/OkanaganSyllabusResourceSeeder.php
@@ -69,6 +69,11 @@ class OkanaganSyllabusResourceSeeder extends Seeder
             'title' => 'Safewalk',
         ]);
 
+        OkanaganSyllabusResource::create([
+            'id_name' => 'ombud',
+            'title' => 'Office of the Ombudperson',
+        ]);
+
 
     }
 }
