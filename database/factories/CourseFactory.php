1f8e2cc6bf100a2e8408744475a45834aba8e921
Michael Ogden <michaelogden8@gmail.com>
2023-06-20 16:07:59 -0700

    Testing courses.store

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
+
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $delivery_modalities=['O','B','I'];
+        $semesters=['W1','W2','S1','S2'];
+
+        return [
+            'course_code' => $this->faker->asciify('****'),
+            'course_num' => $this->faker->randomNumber($nbDigits = 3, $strict=true),
+            'delivery_modality' => $delivery_modalities[array_rand($delivery_modalities)],
+            'year' => $this->faker->year,
+            'semester' => $semesters[array_rand($semesters)],
+            'course_title' => $this->faker->asciify('********************'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'assigned' => 1,
+            'type' => 'unassigned',
+
+        ];
+    }
+}
---
+++ tests/Feature/CourseTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Course;
+use Carbon\Carbon;
+
+class CourseTest extends TestCase
+{
+    /**
+     * 
+     *
+     * @return void
+     */
+
+    public function test_course(){
+
+        $delivery_modalities=['O','B','I'];
+        $semesters=['W1','W2','S1','S2'];
+
+        $user = User::factory()->count(1)->make();
+        $user = User::first();
+        //Need to use real user in DB for this to work
+
+        
+
+        $response=$this->actingAs($user)->post(route('courses.store'), [
+            'course_code' => 'TEST',
+            'course_num' => '111',
+            'delivery_modality' => $delivery_modalities[array_rand($delivery_modalities)],
+            'course_year' => 2022,
+            'course_semester' => $semesters[array_rand($semesters)],
+            'course_title' => 'Intro to Testing',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'assigned' => 1,
+            'type' => 'unassigned',
+            'standard_category_id' => 1,
+            'scale_category_id' => 1
+        ]);
+
+
+        $response->assertRedirect('/');
+        
+        /*
+        //$this->be($user);
+        $course=Course::factory(1)->create();
+        //dd($course);
+        $response= $this->post(route('courses.store', $course));
+        $response->assertRedirectTo(route('courseWizard.step1', $course->course_id));
+        //$this->assertTrue();
+        */
+    }
+}
