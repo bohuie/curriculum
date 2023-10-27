9654e25cfb742f461e0ac795b28e27ccb32b321a
Ogden <71397057+mogden8@users.noreply.github.com>
2023-10-27 10:21:10 -0700

    Finished Test_User_Login

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Invite;
 
 class UserTest extends TestCase
 {
@@ -38,7 +39,38 @@ class UserTest extends TestCase
         ]);
 
         User::where('email', 'test.register@ubc.ca')->delete();
+
     }
+
+    public function test_login_user(){
+        $response=$this->post(route('login'), [
+            "email" => "test.register@ubc.ca",
+            "password" => "password",
+        ]);
+    }
+    /*
+    public function testVerifyEmailValidatesUser(): void
+    {
+        // VerifyEmail extends Illuminate\Auth\Notifications\VerifyEmail in this example
+        $notification = new Invite();
+        $user = User::where('email', 'test.register@ubc.ca')->first();
+    
+        // New user should not has verified their email yet
+        $this->assertFalse($user->hasVerifiedEmail());
+    
+        $mail = $notification->toMail($user);
+        $uri = $mail->actionUrl;
+    
+        // Simulate clicking on the validation link
+        $this->actingAs($user)
+            ->get($uri);
+    
+        // User should have verified their email
+        $this->assertTrue(User::find($user->id)->hasVerifiedEmail());
+
+        User::where('email', 'test.register@ubc.ca')->delete();
+    }
+    */
 
 
 
